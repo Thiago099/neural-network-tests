c979ba3efc9a54abb0689df41b263d4871e4b0e7
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2022-12-13 21:04:52 -0300

    [feature]

---
+++ src/main copy 3.jsx
@@ -0,0 +1,71 @@
+
+function print(...text)
+{
+    new element("div")
+  .parent(document.body)
+  .html(text.join(", "))
+}
+// Define the SGD function
+function sgd(x,y,weights, learningRate) {
+  // Make a prediction using the current weights and biases
+  for(var i = 0; i < x.length; i++)
+  {
+    // make the prediction
+    const prediction = predict(x[i],weights)
+    // calculate the error
+    const error = []
+    for(var j = 0; j < prediction.length; j++)
+    {
+      error[j] = y[i][j] - prediction[j]
+    }
+    // update the weights
+    for(var j = 0; j < weights.length; j++)
+    {
+        for(var k = 0; k < weights[j].length; k++)
+        {
+          weights[j][k] += learningRate * error[j] * x[i][k]
+        }
+    }
+
+  }
+}
+function initializeWeights(x,y)
+{
+  const weights = new Array(y[0].length).fill(0).map(()=> new Array(x[0].length).fill(1))
+  return weights
+}
+function predict(x,weights)
+{
+  var y = []
+  for(var i = 0; i < weights.length; i++)
+  {
+    y[i] = 0
+    for(var j = 0; j < weights[i].length; j++)
+    {
+      y[i] += weights[i][j] * x[j]
+    }
+  }
+  return y
+  // let y1hat = x1 * w1 + x2 * w2 + b1;
+  // let y2hat = x1 * w3 + x2 * w4 + b2;
+  // return [y1hat, y2hat]
+}
+const x =[[1, 0], [0, 1]]
+const y =[[0, 1], [1, 0]]
+const weights = initializeWeights(x,y)
+console.log(weights)
+// Train the model using the SGD function
+for (let i = 0; i < 1000; i++) {
+  // Use a small learning rate
+
+  for(let j = 0; j < x.length; j++)
+  {
+    let learningRate = 0.1;
+    // Run the SGD function to update the weights and biases
+    sgd(x, y, weights, learningRate);
+  }
+}
+
+// Use the trained model to make a prediction
+
+print(predict([1, 0], weights))
--- src/main.jsx
+++ src/main.jsx
@@ -1,71 +1,95 @@
+// import tf
+import * as tf from '@tensorflow/tfjs'
+import * as mnist from 'mnist'
+import './style.css'
 
-function print(...text)
+async function main()
 {
-    new element("div")
-  .parent(document.body)
-  .html(text.join(", "))
-}
-// Define the SGD function
-function sgd(x,y,weights, learningRate) {
-  // Make a prediction using the current weights and biases
-  for(var i = 0; i < x.length; i++)
-  {
-    // make the prediction
-    const prediction = predict(x[i],weights)
-    // calculate the error
-    const error = []
-    for(var j = 0; j < prediction.length; j++)
-    {
-      error[j] = y[i][j] - prediction[j]
-    }
-    // update the weights
-    for(var j = 0; j < weights.length; j++)
-    {
-        for(var k = 0; k < weights[j].length; k++)
-        {
-          weights[j][k] += learningRate * error[j] * x[i][k]
-        }
-    }
+const dataset = mnist.set(99999, 0);
+const x = dataset.training.map((d) => d.input);
 
+
+// Create a model for the autoencoder
+
+
+
+const encoder = tf.sequential();
+encoder.add(tf.layers.dense({ inputShape: [28 * 28], units: 128, activation: 'relu' }));
+encoder.add(tf.layers.dense({ units: 128, activation: 'relu' }));
+encoder.add(tf.layers.dense({ units: 64, activation: 'relu' }));
+
+const decoder = tf.sequential();
+decoder.add(tf.layers.dense({ inputShape:64, units: 64, activation: 'relu' }));
+decoder.add(tf.layers.dense({ units: 128, activation: 'relu' }));
+decoder.add(tf.layers.dense({ units: 28 * 28, activation: 'sigmoid' }));
+
+const model = tf.sequential();
+model.add(encoder);
+model.add(decoder);
+
+// Compile the model
+model.compile({
+  loss: 'meanSquaredError',
+  optimizer: 'adam'
+});
+
+const progress = new element("div")
+.parent(document.body)
+// Train the model on the MNIST dataset
+await model.fit(tf.tensor(x), tf.tensor(x), {
+  epochs: 5,
+  batchSize: 32,
+  callbacks: {
+    onEpochEnd: (epoch, log) => {
+      progress.html(`Epoch ${epoch+1}: loss = ${log.loss}`)
+    }
   }
-}
-function initializeWeights(x,y)
+  //batch
+
+
+});
+
+
+
+// get the encoded image
+
+const encoded  = encoder.predict(tf.tensor([x[0]]));
+
+// get the decoded image
+
+const decoded = decoder.predict(encoded);
+displayImage(decoded.dataSync());
+displayImage(x[0]);
+
+
+
+function displayImage(mnistArray)
 {
-  const weights = new Array(y[0].length).fill(0).map(()=> new Array(x[0].length).fill(1))
-  return weights
-}
-function predict(x,weights)
-{
-  var y = []
-  for(var i = 0; i < weights.length; i++)
-  {
-    y[i] = 0
-    for(var j = 0; j < weights[i].length; j++)
-    {
-      y[i] += weights[i][j] * x[j]
+
+    // Create a canvas element
+    var canvas = document.createElement("canvas");
+    canvas.width = 28;
+    canvas.height = 28;
+    var ctx = canvas.getContext("2d");
+    var imgData = ctx.createImageData(28, 28);
+
+    // Fill the canvas with the MNIST image
+    for (var i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i + 0] = mnistArray[i / 4] * 255;
+        imgData.data[i + 1] = mnistArray[i / 4] * 255;
+        imgData.data[i + 2] = mnistArray[i / 4] * 255;
+        imgData.data[i + 3] = 255;
     }
-  }
-  return y
-  // let y1hat = x1 * w1 + x2 * w2 + b1;
-  // let y2hat = x1 * w3 + x2 * w4 + b2;
-  // return [y1hat, y2hat]
-}
-const x =[[1, 2], [2, 1]]
-const y =[[2, 1], [1, 2]]
-const weights = initializeWeights(x,y)
-console.log(weights)
-// Train the model using the SGD function
-for (let i = 0; i < 1000; i++) {
-  // Use a small learning rate
+    ctx.putImageData(imgData, 0, 0);
 
-  for(let j = 0; j < x.length; j++)
-  {
-    let learningRate = 0.1;
-    // Run the SGD function to update the weights and biases
-    sgd(x, y, weights, learningRate);
-  }
+    // Display the canvas
+    document.body.appendChild(canvas);
+
 }
 
-// Use the trained model to make a prediction
+// print the encoded image
 
-print(predict([1, 2], weights))
+}
+
+main()
+
